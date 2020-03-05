4465a83febac282d4668240fe6a0a953bfc5e84c
root <root@Sudoerz.localdomain>
2020-03-05 15:50:21 +0800

    del

--- prebuilt/10/rw-system.add.sh
+++
@@ -1,3 +0,0 @@
-mount -o bind /mnt/phh/empty_dir /vendor/overlay/DisplayCutoutEmulationTall || true
-mount -o bind /mnt/phh/empty_dir /vendor/overlay/DisplayCutoutEmulationDouble || true
-mount -o bind /mnt/phh/empty_dir /vendor/overlay/DisplayCutoutEmulationCorner || true
